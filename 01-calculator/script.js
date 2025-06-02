3e3547e7abcb8b0399308510715ddb7a1881fc6b
Christopher Lietz <office@zb-lietz.at>
2025-06-02 22:36:01 +0200

    diverese Änderungen

--- 01-calculator/script.js
+++ 01-calculator/script.js
@@ -1,1 +1,17 @@
 // JavaScript Code für Calculator
+//alert("Ich bin da!");
+function Anzeige (name) {
+    alert(name);
+    let AusgabeAlt = document.getElementsByName("ausgabe").values;
+    if (AusgabeAlt = "") {
+        AusgabeAlt = "0";
+    };
+    let Eingabe = document.getElementsByName(name).values;
+    let AusgabeNeu = toString(AusgabeAlt) + toString(Eingabe);
+    alert(AusgabeAlt);
+    alert(Eingabe);
+    alert(AusgabeNeu);
+    document.getElementsByName("ausgabe").values = AusgabeNeu;
+
+    
+}
