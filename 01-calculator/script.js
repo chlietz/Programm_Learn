821d95544856ca2972994038cb80c0e9268b6ed6
Christopher Lietz <office@zb-lietz.at>
2025-06-03 16:28:34 +0200

    Ausgabe weiter entwickelt

--- 01-calculator/script.js
+++ 01-calculator/script.js
@@ -1,17 +1,18 @@
 // JavaScript Code für Calculator
 //alert("Ich bin da!");
 function Anzeige (name) {
-    alert(name);
-    let AusgabeAlt = document.getElementsByName("ausgabe").values;
-    if (AusgabeAlt = "") {
-        AusgabeAlt = "0";
-    };
-    let Eingabe = document.getElementsByName(name).values;
-    let AusgabeNeu = toString(AusgabeAlt) + toString(Eingabe);
-    alert(AusgabeAlt);
-    alert(Eingabe);
-    alert(AusgabeNeu);
-    document.getElementsByName("ausgabe").values = AusgabeNeu;
+    //alert(name);
+    let AusgabeAlt = document.getElementsByName('Ausgabe').value;
+    if (AusgabeAlt == undefined) {
+        AusgabeAlt = '0';
+    }
+    //let Eingabe = document.getElementsByName(name).values;
+    //alert(Eingabe);
+    let AusgabeNeu = AusgabeAlt + name;
+    //alert(AusgabeAlt);
+    
+    //alert(AusgabeNeu);
+    document.getElementById('ausgabe').value = AusgabeNeu;
 
     
 }
