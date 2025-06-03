03b783fd4bf0fcc336b695160108d360c8714848
Christopher Lietz <office@zb-lietz.at>
2025-06-03 21:51:02 +0200

    Es geht weiter

--- 01-calculator/script.js
+++ 01-calculator/script.js
@@ -1,18 +1,65 @@
 // JavaScript Code für Calculator
-//alert("Ich bin da!");
-function Anzeige (name) {
-    //alert(name);
-    let AusgabeAlt = document.getElementsByName('Ausgabe').value;
-    if (AusgabeAlt == undefined) {
-        AusgabeAlt = '0';
-    }
-    //let Eingabe = document.getElementsByName(name).values;
-    //alert(Eingabe);
-    let AusgabeNeu = AusgabeAlt + name;
-    //alert(AusgabeAlt);
-    
-    //alert(AusgabeNeu);
-    document.getElementById('ausgabe').value = AusgabeNeu;
+function initial () {
+    document.getElementById('ausgabe').value = "";
+}
 
-    
+function Anzeige (name) {
+    let AusgabeAlt = Lesen ();
+    let AusgabeNeu = AusgabeAlt + name;
+    Schreiben (AusgabeNeu);  
+}
+
+function Lesen () {
+    let AusgabeAlt = document.getElementById('ausgabe').value;
+    if (AusgabeAlt == undefined) {
+        AusgabeAlt = '';
+    };
+
+    return AusgabeAlt;
+}
+
+function Schreiben (name) {
+    document.getElementById('ausgabe').value = name;
+}
+
+function Rechnen (zeichen) {
+    let mal1;
+    let mal2;
+    let erg;
+    //let mal1 = Lesen();
+    //alert(mal1 + ' - ' + zeichen);
+    switch (zeichen) {
+        case "Tmal":
+            mal1 = Lesen();
+            alert(mal1);
+            initial();
+            break;
+        case "Tdiv":
+
+            break;
+        case "Tplus":
+
+            break;
+
+        case "Tminus":
+
+            break;
+
+        case "Tres":
+            mal2 = Lesen();
+            alert(mal1);
+            alert(mal2);
+            if ( zeichen = "Tmal") {
+                erg = parseInt(mal1) * parseInt(mal2);
+                //alert(erg);
+                Schreiben(erg);
+            }
+            break;
+        
+        case "Tz":
+
+            break;
+
+
+    }
 }
