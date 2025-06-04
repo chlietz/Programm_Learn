abd209e9c13cabb55a585ab3bc456ac4996ca619
Christopher Lietz <office@zb-lietz.at>
2025-06-04 22:37:55 +0200

    Neue Gedanken

--- 01-calculator/script.js
+++ 01-calculator/script.js
@@ -1,37 +1,41 @@
 // JavaScript Code für Calculator
+
+//Anzeige löschen
 function initial () {
     document.getElementById('ausgabe').value = "";
 }
 
+// Zahlen für die Ausgabe zusammenstellen
 function Anzeige (name) {
     let AusgabeAlt = Lesen ();
     let AusgabeNeu = AusgabeAlt + name;
     Schreiben (AusgabeNeu);  
 }
 
+// Inhalt der Anzeige auslesen
 function Lesen () {
     let AusgabeAlt = document.getElementById('ausgabe').value;
     if (AusgabeAlt == undefined) {
         AusgabeAlt = '';
     };
-
     return AusgabeAlt;
 }
 
+// Inhalt in die Anzeige schreiben
 function Schreiben (name) {
     document.getElementById('ausgabe').value = name;
 }
 
-function Rechnen (zeichen) {
-    let mal1;
-    let mal2;
-    let erg;
-    //let mal1 = Lesen();
-    //alert(mal1 + ' - ' + zeichen);
+//Verteiler wenn eine Operationstaste (x/+- etc.) geklickt wurde
+function Verteiler (zeichen) {
+    var mal1 = 0;
+    var mal2 = 0;
+    var erg = 0;
+    
+
     switch (zeichen) {
         case "Tmal":
             mal1 = Lesen();
-            alert(mal1);
             initial();
             break;
         case "Tdiv":
@@ -47,8 +51,6 @@ function Rechnen (zeichen) {
 
         case "Tres":
             mal2 = Lesen();
-            alert(mal1);
-            alert(mal2);
             if ( zeichen === "Tmal") {
                 erg = parseInt(mal1) * parseInt(mal2);
                 //alert(erg);
