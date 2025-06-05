3d19f41b933188818eb452bdfbb94ed3054c55f8
Christopher Lietz <office@zb-lietz.at>
2025-06-05 21:41:17 +0200

    nun ganz anders

--- 01-calculator/script.js
+++ 01-calculator/script.js
@@ -1,4 +1,8 @@
 // JavaScript Code für Calculator
+var faktoren = ['', ''];
+let i = 0;
+let operator = null;
+let warteAufZahl = false;
 
 //Anzeige löschen
 function initial () {
@@ -6,62 +10,66 @@ function initial () {
 }
 
 // Zahlen für die Ausgabe zusammenstellen
-function Anzeige (name) {
-    let AusgabeAlt = Lesen ();
-    let AusgabeNeu = AusgabeAlt + name;
-    Schreiben (AusgabeNeu);  
-}
-
-// Inhalt der Anzeige auslesen
-function Lesen () {
-    let AusgabeAlt = document.getElementById('ausgabe').value;
-    if (AusgabeAlt == undefined) {
-        AusgabeAlt = '';
-    };
-    return AusgabeAlt;
-}
-
-// Inhalt in die Anzeige schreiben
-function Schreiben (name) {
-    document.getElementById('ausgabe').value = name;
-}
-
-//Verteiler wenn eine Operationstaste (x/+- etc.) geklickt wurde
-function Verteiler (zeichen) {
-    var mal1 = 0;
-    var mal2 = 0;
-    var erg = 0;
-    
-
-    switch (zeichen) {
-        case "Tmal":
-            mal1 = Lesen();
-            initial();
-            break;
-        case "Tdiv":
-
-            break;
-        case "Tplus":
-
-            break;
-
-        case "Tminus":
-
-            break;
-
-        case "Tres":
-            mal2 = Lesen();
-            if ( zeichen === "Tmal") {
-                erg = parseInt(mal1) * parseInt(mal2);
-                //alert(erg);
-                Schreiben(erg);
-            }
-            break;
-        
-        case "Tz":
-
-            break;
-
-
+function Schreiben () {
+    const display = document.getElementById('ausgabe');
+    if (faktoren[1] != "") {
+        display.value = faktoren[1];
+    } else if (faktoren[0] != "") {
+        display.value = faktoren[0];
+    } else {
+        display.value = "0";
     }
+
+}
+
+
+function Anzeige (ziffer) {
+    if (warteAufZahl) {
+        faktoren[1] = ziffer;
+        warteAufZahl = false;
+    } else {
+        if (operator === null) {
+            faktoren[0] += ziffer;
+        } else {
+            faktoren[1] += ziffer
+        }
+    }
+
+    Schreiben();
+}
+
+function EigababeOperator(op) {
+    if (faktoren[0] === "") return;
+
+    operator = op;
+    warteAufZahl = true;
+}
+
+function Berechnen() {
+    if (faktoren[0] === '' || faktoren[1] === '' || operator === null) return;
+    const a = parseFloat(faktoren[0]);
+    const b = parseFloat(faktoren[1]);
+
+    switch (operator) {
+        case "+": faktoren[0] = a + b; 
+        break;
+        case "-": faktoren[0] = a - b; 
+        break;
+        case "*": faktoren[0] = a * b; 
+        break;
+        case "/": faktoren[0] = b !== 0 ? a / b : "Div by Null"; 
+        break;
+    }
+    faktoren[0] = faktoren[0].toString();
+    faktoren[1] = '';
+    warteAufZahl = false;
+    Schreiben();
+}
+
+function ClearDisplay() {
+    document.getElementById('ausgabe').value = '';
+    faktoren[0] = '';
+    faktoren[1] = '';
+    operator = null;
+    warteAufZahl = false;
 }
